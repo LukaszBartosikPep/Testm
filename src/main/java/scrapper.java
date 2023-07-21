b10cb6a820903827b865b97e5253ce9969456545
lbartosik <lbartosik@pepco.eu>
2023-07-21 13:44:39 +0200

    "Web scraper"

--- src/main/java/scrapper.java
+++ src/main/java/scrapper.java
@@ -2,7 +2,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+
+
+import java.io.FileWriter;
 import java.util.List;
+import java.io.BufferedWriter;
+import java.io.IOException;
 public class scrapper {
 
     private WebDriver driver;
@@ -16,21 +21,40 @@ public class scrapper {
 
 
     }
-    public void getElement(){
+    public void getElement() {
         driver.get("https://ksiazkipoangielsku.pl/18-adventure-novels?n=20&id_category=18");
 
 //        String pageTitle = driver.getTitle();
 //        System.out.println("Page title: " + pageTitle);
 //        WebElement element = driver.findElement(By.className("block_content"));
 //        WebElement element = driver.findElement(By.cssSelector("h4"));
-        List<WebElement> elements=driver.findElements(By.tagName("h3"));
+        List<WebElement> elements = driver.findElements(By.tagName("h3"));
 //        String head=element.getAttribute("a href");
 //        System.out.println(head);
 
         for (WebElement elementy : elements) {
             System.out.println(elementy.getText());
+            String saved = elementy.getText();
         }
 
         driver.quit();
+
     }
+        public void saveText() {
+            try {
+                String filePath = "Data.txt";
+                scrapper scrapper = new scrapper();
+                FileWriter file = new FileWriter(filePath);
+                BufferedWriter buffer = new BufferedWriter(file);
+
+                buffer.write(saved);
+            }catch(IOException e){
+             e.printStackTrace();
+            }
+
+
+
+
+        }
+
 }
